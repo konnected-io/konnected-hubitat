8f3118a11b634bc9874afe2caa995af86d1ff6a0
Nate Clark <nate@nateclark.com>
2020-08-12 22:05:21 -0400

    update version numbers

--- apps/konnected-pro-device.groovy
+++ apps/konnected-pro-device.groovy
@@ -13,7 +13,7 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
-public static String version() { return "2.2.6" }
+public static String version() { return "1.0.0" }
 
 definition(
   name:        "Konnected Pro Device",
